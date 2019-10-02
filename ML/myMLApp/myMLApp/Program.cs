47d229ff1777f83c953e62107cc02652b899eb87
fenglui <3090641@qq.com>
2019-10-02 21:54:46 +0800

    fix

---
+++ ML/myMLApp/myMLApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.ML;
+using MyMLAppML.Model.DataModels;
+using System;
+
+namespace myMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ConsumeModel();
+        }
+
+        public static void ConsumeModel()
+        {
+            // Load the model
+            MLContext mlContext = new MLContext();
+            ITransformer mlModel = mlContext.Model.Load("MLModel.zip", out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            // Use the code below to add input data
+            var input = new ModelInput();
+            input.Sentiment = true;
+            input.SentimentText = "This game is so dang good man";
+
+            // Try model on sample data
+            ModelOutput result = predEngine.Predict(input);
+            Console.WriteLine(result.Score);
+            Console.ReadLine();
+        }
+    }
+}
