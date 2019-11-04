56a04e02ec077533976ab505a50740154cd994af
fenglui <3090641@qq.com>
2019-11-04 18:58:47 +0800

    add buefy project

---
+++ buefy/nuxt.config.js
@@ -0,0 +1,9 @@
+{
+    modules: [
+        // Simple usage
+        'nuxt-buefy',
+
+        // Or you can customize
+        ['nuxt-buefy', { css: false, materialDesignIcons: false }],
+    ]
+}
---
+++ buefy/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <a href="https://bulma.io/documentation/layout/container/" target="_blank">
+        bulma
+    </a>
+    
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
